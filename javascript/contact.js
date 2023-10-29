1384185d3fab2d9928dbba4d8423442af1a50f3a
Anthony Alam <anthony.alam2005@gmail.com>
2023-10-28 23:58:07 -0400

    more styling for contact page and creation of contact.js

---
+++ javascript/contact.js
@@ -0,0 +1,7 @@
+function checkCircleBox() {
+    //Check for selection-circle class
+        //if the selected-circle class exists (I still need to create this, go to figma)
+            //remove the selected-circle class
+        //else 
+            //add the selected-circle class
+    }       
