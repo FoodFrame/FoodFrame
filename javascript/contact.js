9f49509f51f23bb687f2d462cbf1e25922331e2b
Anthony Alam <anthony.alam2005@gmail.com>
2023-10-29 00:46:08 -0400

    JS for selection of member to speak to

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,4 +1,11 @@
-function checkCircleBox() {
+function checkCircleBox(listName) {
+    const selectionCircle = document.getElementById(`${listName}`);
+    if (selectionCircle.classList.contains('selected-circle') == false) {
+        selectionCircle.classList.add('selected-circle');
+    }
+    else {
+        selectionCircle.classList.remove('selected-circle');
+    }
     //Check for selection-circle class
         //if the selected-circle class exists (I still need to create this, go to figma)
             //remove the selected-circle class
