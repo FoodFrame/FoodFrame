db0e416563780ef2fcc99b7fda7813873bb5b603
Anthony Alam <anthony.alam2005@gmail.com>
2023-11-12 19:34:04 -0500

    trying to figure out the contact page

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,10 +1,14 @@
 function checkCircleBox(listName) {
-    const selectionCircle = document.getElementById(`${listName}`);
-    if (selectionCircle.classList.contains('selected-circle') == false) {
-        selectionCircle.classList.add('selected-circle');
+    const selectionCircle = document.querySelectorAll(`#${listName}`);
+    if (selectionCircle[0].classList.contains('selected-circle') == false) {
+        for (let i = 0; i < selectionCircle.length; i++) {
+            selectionCircle[i].classList.add('selected-circle');
+        }   
     }
     else {
-        selectionCircle.classList.remove('selected-circle');
+        for (let i = 0; i < selectionCircle.length; i++) {
+            selectionCircle[i].classList.remove('selected-circle');
+        }     
     }
     //Check for selection-circle class
         //if the selected-circle class exists (I still need to create this, go to figma)
